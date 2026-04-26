1af98896f18e93ba97e70febdd961c7d4c2fac4b
bhavesh-sagar <bhaveshnt9@gmail.com>
2026-04-26 16:44:28 +0530

    adding Redis caching for faster responses

---
+++ app/services/cache_service.py
@@ -0,0 +1,17 @@
+from app.dependencies import redis_client
+import json
+
+def get_cache(key):
+    try:
+        data = redis_client.get(key)
+        return json.loads(data) if data else None
+    except Exception as e:
+        print(" Redis get error:", str(e))
+        return None
+
+
+def set_cache(key, value, ttl=3600):
+    try:
+        redis_client.setex(key, ttl, json.dumps(value))
+    except Exception as e:
+        print(" Redis set error:", str(e))
