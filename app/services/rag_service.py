3c3b936cfc386305ac272e274333eb7ab2aa5105
bhavesh-sagar <bhaveshnt9@gmail.com>
2026-04-27 12:08:15 +0530

    fix: remove embed_model after lightweight optimization

--- app/services/rag_service.py
+++ app/services/rag_service.py
@@ -1,14 +1,15 @@
-from app.dependencies import qdrant, embed_model
+# from app.dependencies import qdrant, embed_model
+from app.dependencies import qdrant
 from app.services.ner_service import extract_location
 
 def hybrid_search(query):
     try:
         location = extract_location(query)
-        vector = embed_model.encode(query).tolist()
+        # vector = embed_model.encode(query).tolist()
 
         results = qdrant.query_points(
             collection_name="travel_data",
-            query=vector,
+            # query=vector,
             limit=5
         )
 
